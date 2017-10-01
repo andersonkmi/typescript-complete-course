57a1011429bcab54bf32e0c5a8bc4a80b795dd08
Anderson Ito <andersonkmi@acm.org>
2017-10-01 18:12:13 -0300

    Exercise added

---
+++ exercise.js
@@ -0,0 +1,13 @@
+var bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+var myself = {
+    name: "Max",
+    bankAccount: bankAccount,
+    hobbies: ["Sports", "Cooking"]
+};
+myself.bankAccount.deposit(300);
+console.log(myself);
